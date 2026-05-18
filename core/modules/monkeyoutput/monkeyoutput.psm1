9fcdec93e0b04f59479e4acd932b6214bb6421ef
silverhack <diariode@gmail.com>
2026-05-18 14:04:31 +0200

    update monkeyoutput

--- core/modules/monkeyoutput/monkeyoutput.psm1
+++ core/modules/monkeyoutput/monkeyoutput.psm1
@@ -1,12 +1,38 @@
 Set-StrictMode -Version Latest #-Version 1.0
 
+#Import monkey utils
+$modulesRoot = Split-Path -Parent $PSScriptRoot
+$monkeyutils = Join-Path $modulesRoot 'monkeyutils/monkeyutils.psd1'
+If (-not (Get-Module -Name 'monkeyutils')) {
+    Import-Module $monkeyutils
+}
+
+#Import psmarkdig module
+$markdownPsModule = Join-Path $modulesRoot 'psmarkdig/psmarkdig.psd1'
+If (-not (Get-Module -Name 'psmarkdig')) {
+    Import-Module $markdownPsModule
+}
+
+# Import localized data
 $LocalizedDataParams = @{
     BindingVariable = 'messages';
-    BaseDirectory = "{0}/{1}" -f $PSScriptRoot, "Localized";
+    BaseDirectory = (Join-Path $PSScriptRoot 'Localized');
 }
 #Import localized data
 Import-LocalizedData @LocalizedDataParams;
 
-$listofFiles = [System.IO.Directory]::EnumerateFiles(("{0}" -f $PSScriptRoot),"*.ps1","AllDirectories")
-$all_files = $listofFiles.Where({($_ -like "*public*") -or ($_ -like "*private*")})
-$all_files.ForEach({. $_})
+#Import public and private files
+$sourceFolders = @('private', 'public')
+ForEach ($folder in $sourceFolders) {
+    $path = Join-Path $PSScriptRoot $folder
+    If (-not (Test-Path $path)) {
+        continue
+    }
+    $files = [System.IO.Directory]::EnumerateFiles($path,'*',[System.IO.SearchOption]::AllDirectories)
+    ForEach ($file in ($files | Sort-Object)) {
+        If ([System.IO.Path]::GetExtension($file) -ne '.ps1') {
+            continue
+        }
+        . $file
+    }
+}
